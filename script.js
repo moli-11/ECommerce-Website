f6579ed49b4cb4e80dd382d90506cde3e206a4de
Moli Shah <shah.moli98@gmail.com>
2023-02-22 03:54:47 +0530

    Uploaded files

---
+++ script.js
@@ -0,0 +1,26 @@
+//navbar
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click', ( ) => {
+        nav.classList.add('active');
+    }) 
+}
+
+if(close){
+    close.addEventListener('click', ( ) => {
+        nav.classList.remove('active');
+    }) 
+}
+
+//sproduct - wishlist
+const wish = document.getElementById('wishlist');
+
+if(wish){
+    wish.addEventListener('click', ( ) => {
+        wish.classList.remove('wish');
+        wish.classList.add('normal');
+    }) 
+}
